64211ee8940aa8d5e4bd7262f7811e60b2bb4a09
saravanakumara <saravanakumarssm@gmail.com>
2026-02-27 22:47:38 +0530

    Abstract1

---
+++ src/core/AbstractClass.java
@@ -0,0 +1,14 @@
+package core;
+
+public abstract class AbstractClass {
+
+	public abstract void obstractTest();
+	public static void nonAbstract() {
+		System.out.println("Non abstract method.");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		nonAbstract();
+	}
+
+}
--- src/core/StaticBlock.java
+++ src/core/StaticBlock.java
@@ -1,6 +1,6 @@
 package core;
 
-public class StaticBlock {
+public class StaticBlock { //private, protected are not allowed for class but final, abstract, public, default are allowed
 	int a = 10;
 	static int b;
 	//Static block will be excuted before running main().
@@ -8,16 +8,34 @@ public class StaticBlock {
 	static {
 		b = 20;
 		//a = 30; //Only static variable is accessed in the static method or block
-		System.out.println("Static block.");
+		System.out.println("Static block - variable value: " + b);
 	}
 	
-	private StaticBlock() {
+	static final int c = 100;
+	final int d; //not initialized
+	//d = 5; error when initialized other than constructor
+	//static {
+	//	c = 18; //final variable can not be assigned in static block
+	//}
+	
+	private StaticBlock() { //Can not be static, final and abstract
+		this.d = 0;
 		System.out.println("Default constructor.");
 	}
+	
 	public static void main(String[] args) {
+		b = 30;
+		System.out.println("Static variable value after update in main() - " + b);
 		System.out.println("main method.");
 		StaticBlock sbO = new StaticBlock();
 		System.out.println("private contructor used to access the property in the same class. " + sbO.a);
+		System.out.println("Final variable value: " + c);
+		//c = c + 10;//final can not be changed
+		
+	}
+	
+	public static void main(String[] args, String str) {
+		System.out.println("main method overload.");
 	}
 
 }
