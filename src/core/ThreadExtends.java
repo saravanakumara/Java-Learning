c1602d63961fd66538f40d36fd49062fea564585
saravanakumara <saravanakumarssm@gmail.com>
2026-03-02 15:57:55 +0530

    threads with threadpool

--- src/core/ThreadExtends.java
+++ src/core/ThreadExtends.java
@@ -36,9 +36,9 @@ public class ThreadExtends {
 
 }
 
-//Thread is an engine -> Kitchen
+//Thread is an engine -> Kitchen or worker
 //Runnable is a task -> chef
-//Thread can be invoked only once and not invoked next time
+//Thread can be invoked only once and not invoked next time and if invoke 2nd time IllegalThreadStateException will occur.
 //Class can be completely turned into Thread by extends. limits inheritance. 
 //Mostly will use runnable interface
 //Runnable interface will have only one run method and it needs engine that is thread class which used to drive the task
@@ -46,10 +46,21 @@ public class ThreadExtends {
 //Process is heavy and each process have own memory space and context switch will take more cost.
 //Multi tasking - multiple task can be performed simultaneously
 //2 ways to achieve multi threading, multi processing
-//each thread runs separate and have own stack.
+//start() -> will tell JVM and allocate its own stack. each thread runs separate and have own stack.
 //race condition -> all threads access the same shared varaible which will give inconsitent/lose of data
 //deadlock -> if thread A wants to access from ThreadB but threadB wants to access theradA, each will depend each other and stalls
 //Thread.sleep() -> current thread will be moved to suspended state for the time specified and blocks the execution
 //t1.wait() -> synchronized block usually acquires lock for a thread and will not allow other thread to access until other thread complets its work.
 //t1.notify() -> it will notify the waiting thread to resume its work. Producer/consumer.
 //t1.join() -> wait for the other thread to complete. Main thread will wait for the child thread to complete its work.
+//Thread states -> new -> start -> runnable -> running -> suspended -> terminated (stop or error or finished)
+//Thread states -> new -> start -> runnable (start) -> running (cpu will allocate time) -> suspended (sleep) -> resume (runnable & running) -> terminated (stop or error or finished)
+//In concurrency, 
+//1. ThreadLocal -> isolates the variable for the thread
+//2. ExecutorService ex = Executor.newFixedThreadPool(5); ex.execute(() -> perform task);
+//Priorities can be given to the thread. min - 1, normal - 5, max - 10
+//Priority based thread execution depends on thread schedulers - preemptive, time slicing, first come first serve 
+//Deamon thread -> It provides service thread to the user thread to run a job in background. life of this depends on the user thread, if no user thread then this thread will be killed by JVM
+//ThreadPool -> group of threads created and reused for task, no need to create new threads again and again. A thread
+//from the thread pool is pulled out and assigned a job by the service provider. newFixedThreadPool(5).
+//
